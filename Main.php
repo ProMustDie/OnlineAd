71e6a20c6b5393f73d31447bf836459bc907052d
Yo Wen <teohyowen2003@gmail.com>
2023-10-16 14:05:22 +0800

    made a footer file, admin html

--- Main.php
+++ Main.php
@@ -6,8 +6,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
     <link href="CSS/main.css" rel="stylesheet">
 
 
@@ -85,7 +83,7 @@
 
 
                     <div class="card m-3" style="width: 30rem;">
-                        <div class="ImgContainer m-3">
+                        <div class="ImgContainer m-2">
                             <img src="img/logo.png" class="imgSize card-img-top img-fluid" alt="..." id="myImg" onclick="openModal('img/logo.png', 'Lorem4000')">
                         </div>
                         <div class="card-body">
@@ -93,13 +91,25 @@
                             <p class="card-text" id="TextSub">This is a wider card with supporting text below as a
                                 natural lead-in to
                                 additional content. This content is a little bit longer.</p>
+
+
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"></li>
+                                <li class="list-group-item">
+                                    <div class="d-flex justify-content-end m-2">
+                                        <a href="#" class="btn btn-outline-danger">Delete</a>
+                                    </div>
+                                </li>
+                            </ul>
+
+
                             <p class="card-text" id="TextTime"><small class="text-muted">Last updated 3 mins ago</small>
                             </p>
                         </div>
                     </div>
 
                     <div class="card m-3" style="width: 30rem;">
-                        <div class="ImgContainer m-3">
+                        <div class="ImgContainer m-2">
                             <img src="img/TheSun.jpeg" class="imgSize card-img-top img-fluid" alt="..." id="myImg" onclick="openModal('img/TheSun.jpeg', 'Snow')">
                         </div>
                         <div class="card-body">
@@ -107,6 +117,18 @@
                             <p class="card-text" id="TextSub">This is a wider card with supporting text below as a
                                 natural lead-in to
                                 additional content. This content is a little bit longer.</p>
+
+
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"></li>
+                                <li class="list-group-item">
+                                    <div class="d-flex justify-content-end m-2">
+                                        <a href="#" class="btn btn-outline-danger">Delete</a>
+                                    </div>
+                                </li>
+                            </ul>
+
+
                             <p class="card-text" id="TextTime"><small class="text-muted">Last updated 3 mins ago</small>
                             </p>
                         </div>
@@ -128,54 +150,17 @@
 
     <!--Main Body-->
 
-
-    <!--Footer-->
-
-    <footer class="footer bg-dark text-center text-white  mt-5">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Social media -->
-            <section class="mb-4">
-                <!-- Facebook -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/thesundaily" role="button"><i class="bi bi-facebook"></i></a>
-
-                <!-- Twitter -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/thesundaily" role="button"><i class="bi bi-twitter-x"></i></a>
-
-                <!-- Google -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/thesundaily/" role="button"><i class="bi bi-instagram"></i></a>
-
-                <!-- Instagram -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/channel/UCJNLiW1NkgyHeoijxH-a_Wg" role="button"><i class="bi bi-youtube"></i></a>
-
-                <!-- Linkedin -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.tiktok.com/@thesundaily?lang=en" role="button"><i class="bi bi-tiktok"></i></a>
-
-                <!-- Github -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://t.me/thesuntelegram" role="button"><i class="bi bi-telegram"></i></a>
-
-            </section>
-            <!-- Section: Social media -->
-        </div>
-        <!-- Grid container -->
-
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
-            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
-
-    <!--Footer-->
-
-
+    <!--footer-->
+    <?php
+    include("Includes/footer.php");
+    ?>
+    <!--footer-->
 
     <!--//!MODAL FOR IMAGES -->
     <div id="myModal" class="modal">
         <span class="close" onclick="closeModal()">&times;</span>
         <img class="modal-content" id="modalImg">
-        <div class="container text-break" id="caption"></div>
+        <div class="container text-break mt-3" id="caption"></div>
     </div>
 
     <script src="JS/MainModal.js"></script>
