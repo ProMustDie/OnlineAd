c1e800ed6dde8ca83a464af1403976634d232b2b
Yo Wen <teohyowen2003@gmail.com>
2023-10-17 14:00:46 +0800

    remove script from navbar n add in other page

--- Register.php
+++ Register.php
@@ -140,3 +140,4 @@ if (isset($_POST["SignUpSubmit"])) {
 </html>
 
 <script src="JS/loginPreference.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
