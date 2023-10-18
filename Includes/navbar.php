e030d16e591b0a7b435fe4dc1befee9ccb4bec7f
Yo Wen <teohyowen2003@gmail.com>
2023-10-18 16:09:01 +0800

    remove html in navbar and change bs into older v in request

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -21,18 +21,9 @@ if (isset($_GET['category'])) {
     $filter = implode(" ", $cat);
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    <link href="CSS/index.css" rel="stylesheet">
-
-</head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+<link href="CSS/index.css" rel="stylesheet">
 
 <body>
 
@@ -113,7 +104,7 @@ if (isset($_GET['category'])) {
 
 
     <!--//!HISTORY MODAL-->
-    <div class="modal fade" id="historyModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1" style="padding-top:10px">
+    <div class="modal " id="historyModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1" style="padding-top:10px">
         <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content modal-xl">
                 <div class="modal-header">
@@ -235,5 +226,3 @@ if (isset($_GET['category'])) {
     <!--//!HISTORY MODAL-->
 
 </body>
-
-</html>
