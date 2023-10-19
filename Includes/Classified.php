f88e19fc1a2e17f0e0e6c5d10a6ab7caf5c72221
Yo Wen <teohyowen2003@gmail.com>
2023-10-19 15:52:02 +0800

    open history modal when refresh.

--- Includes/Classified.php
+++ Includes/Classified.php
@@ -1,23 +1,26 @@
     <?php
-    class Classified{
+    class Classified
+    {
         private $userTable = "users";
         private $adsTable = "ads";
         private $requestTable = "requests";
         private $conn;
 
-        public function __construct(){
+        public function __construct()
+        {
             $db = new DatabaseConnection;
             $this->conn = $db->conn;
         }
 
-        public function getAds($key, $filter, $stat){
+        public function getAds($key, $filter, $stat)
+        {
             $key = $this->conn->real_escape_string($key);
             $filter = $this->conn->real_escape_string($filter);
             $status = "%$stat%";
 
-            if($key == NULL && $filter == NULL){
+            if ($key == NULL && $filter == NULL) {
                 $sql = "SELECT a.AdID, a.AdName, a.AdDescription, a.Price, a.AdAuthorID, a.AdPicture, a.AdCategory, a.AdPostedDateTime, a.AdStatus, u.UserID, u.UserName
-                        FROM " . $this->adsTable . " as a, ". $this->userTable . " as u
+                        FROM " . $this->adsTable . " as a, " . $this->userTable . " as u
                         WHERE a.AdAuthorID = u.UserID
                         AND a.AdStatus LIKE ?
                         ORDER BY a.AdID DESC ";
@@ -26,7 +29,7 @@
                 $stmt->execute();
                 $result = $stmt->get_result();
                 return $result;
-            }else{
+            } else {
                 $sql = "SELECT DISTINCT a.AdID, a.AdName, a.AdDescription, a.Price, a.AdAuthorID, a.AdPicture, a.AdCategory, a.AdPostedDateTime, a.AdStatus, u.UserID, u.UserName
                 FROM " . $this->adsTable . " as a, " . $this->userTable . " as u
                 WHERE (a.AdName LIKE ? OR a.AdDescription LIKE ? OR u.UserName LIKE ?)
@@ -35,24 +38,25 @@
                 AND a.AdAuthorID = u.UserID
                 ORDER BY a.AdID DESC ";
 
-                    $stmt = $this->conn->prepare($sql);
-                    $param = "%$key%";
-                    $category = "%$filter%";
-                    $stmt->bind_param("sssss", $param, $param, $param, $category, $status);
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-                    return $result;
+                $stmt = $this->conn->prepare($sql);
+                $param = "%$key%";
+                $category = "%$filter%";
+                $stmt->bind_param("sssss", $param, $param, $param, $category, $status);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                return $result;
             }
         }
 
-        public function getCategories() {
+        public function getCategories()
+        {
             // Determine the maximum number of words in any category
             $sqlMaxWords = "SELECT MAX(LENGTH(AdCategory) - LENGTH(REPLACE(AdCategory, ' ', '')) + 1) AS max_words FROM ads";
             $stmtMaxWords = $this->conn->prepare($sqlMaxWords);
             $stmtMaxWords->execute();
             $resultMaxWords = $stmtMaxWords->get_result();
             $maxWords = $resultMaxWords->fetch_assoc()["max_words"];
-        
+
             // Generate the dynamic subquery for extracting unique words
             $subquery = "";
             for ($i = 1; $i <= $maxWords; $i++) {
@@ -61,14 +65,14 @@
                     $subquery .= " UNION ALL ";
                 }
             }
-        
+
             // Construct the final SQL query
             $sql = "SELECT word AS Category
                     FROM ($subquery) subquery
                     WHERE word IS NOT NULL AND word != ''
                     GROUP BY word
                     ORDER BY word";
-        
+
             $stmt = $this->conn->prepare($sql);
             $stmt->execute();
             $result = $stmt->get_result();
@@ -77,17 +81,17 @@
             if (isset($_GET['category'])) {
                 $selectedCategories = $_GET['category'];
             }
-        
+
             return array('result' => $result, 'selectedCategories' => $selectedCategories);
         }
 
-        public function deleteAd($AdID){
+        public function deleteAd($AdID)
+        {
             $sqlQuery = "DELETE FROM " . $this->adsTable . " WHERE AdID = ?";
-		    $stmt = $this->conn->prepare($sqlQuery);
-		    $stmt->bind_param("i", $AdID);
-		    $stmt->execute();
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->bind_param("i", $AdID);
+            $stmt->execute();
         }
-        
     }
 
     ?>
--- Request.php
+++ Request.php
@@ -128,9 +128,18 @@
 
     <!--ADMIN -->
 
-    <script src="JS/MainModal.js"></script>
+
+
+
+    <!--footer-->
+    <?php
+    include("Includes/footer.php");
+    ?>
+    <!--footer-->
+
+
+
     <script src="JS/request.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
 </body>
