12ac15b08e81783500835811e62088fab29480f1
Yo Wen <teohyowen2003@gmail.com>
2023-10-20 09:54:32 +0800

    removed entire history from navbar

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,63 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+<footer class="footer bg-dark text-center text-white mt-auto">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0 mt-auto">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/thesundaily" role="button"><i class="bi bi-facebook"></i></a>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/thesundaily" role="button"><i class="bi bi-twitter-x"></i></a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/thesundaily/" role="button"><i class="bi bi-instagram"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/channel/UCJNLiW1NkgyHeoijxH-a_Wg" role="button"><i class="bi bi-youtube"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.tiktok.com/@thesundaily?lang=en" role="button"><i class="bi bi-tiktok"></i></a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://t.me/thesuntelegram" role="button"><i class="bi bi-telegram"></i></a>
+
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+<!--Footer-->
 
 
-    <title>Document</title>
-</head>
-
-<body>
-
-    <footer class="footer bg-dark text-center text-white  mt-5">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Social media -->
-            <section class="mb-4">
-                <!-- Facebook -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/thesundaily" role="button"><i class="bi bi-facebook"></i></a>
-
-                <!-- Twitter -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/thesundaily" role="button"><i class="bi bi-twitter-x"></i></a>
-
-                <!-- Google -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/thesundaily/" role="button"><i class="bi bi-instagram"></i></a>
-
-                <!-- Instagram -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/channel/UCJNLiW1NkgyHeoijxH-a_Wg" role="button"><i class="bi bi-youtube"></i></a>
-
-                <!-- Linkedin -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.tiktok.com/@thesundaily?lang=en" role="button"><i class="bi bi-tiktok"></i></a>
-
-                <!-- Github -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://t.me/thesuntelegram" role="button"><i class="bi bi-telegram"></i></a>
-
-            </section>
-            <!-- Section: Social media -->
-        </div>
-        <!-- Grid container -->
-
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
-            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
-
-    <!--Footer-->
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="JS/modalOpen.js"></script>
-    <script src="JS/MainModal.js"></script>
-
-</body>
-
-</html><!--Footer-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- Includes/navbar.php
+++ Includes/navbar.php
@@ -21,7 +21,7 @@ if (isset($_GET['category'])) {
     $filter = implode(" ", $cat);
 }
 ?>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 <link href="CSS/index.css" rel="stylesheet">
@@ -48,10 +48,10 @@ if (isset($_GET['category'])) {
                             <a class="nav-link" href="Request.php">Request</a>
                         </li>
                         <?php if (isset($_SESSION["authenticated"]) && $_SESSION["authenticated"] == true && $_SESSION['auth_user']['user_type'] != "Admin") : ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" data-bs-target="#historyModal" data-bs-toggle="modal" id="History">History</a>
-                        </li>
-                    
+                            <li class="nav-item">
+                                <a class="nav-link" href="history.php" id="History">History</a>
+                            </li>
+
                         <?php
                         endif;
                         if (isset($_SESSION["authenticated"]) && $_SESSION['authenticated'] ==  true) : ?>
@@ -99,156 +99,5 @@ if (isset($_GET['category'])) {
     </header>
     <!--navbar-->
 
-    <?php if (isset($_SESSION["authenticated"]) && $_SESSION["authenticated"] == true && $_SESSION['auth_user']['user_type'] != "Admin") : ?>
-    <!--//!HISTORY MODAL-->
-    <div class="modal fade" id="historyModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
-        <div class="modal-dialog modal-fullscreen modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content modal-fullscreen">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel">History</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="CloseModal"></button>
-                </div>
-                <div class="modal-body">
-
-
-                    <div class="col bg-light">
-                        <div class="row d-flex justify-content-center">
-                            <?php
-                            $result = $classified->getAds(NULL, NULL, NULL, isset($_SESSION['auth_user'])?$_SESSION['auth_user']['user_id']:NULL); //Pending Review, Rejected Request, Pending Payment, Rejected Payment, Approved, Cancelled, Expired
-                            if (mysqli_num_rows($result) > 0) :
-                                while ($ads = $result->fetch_assoc()) {
-                            ?>
-
-                                    <div class="card m-3" style="width: 30rem;">
-                                        <div class="ImgContainer m-2">
-                                            <img src="<?= $ads['AdPicture'] ?>" class="imgSize card-img-top img-fluid" id="myImg" data-bs-toggle="modal" data-bs-target="#modalImg-<?= $ads['AdID'] ?>">
-                                        </div>
-                                        <div class="card-body">
-                                            <h5 class="card-title fs-3 fw-bold" id="TextHeader"><?= $ads['AdName'] ?></h5>
-                                            <p class="card-text text-truncate" id="TextSub"><?= $ads['AdDescription'] ?></p>
-                                            <ul class="list-group list-group-flush">
-                                                <li class="list-group-item"></li>
-                                                <li class="list-group-item p-0">
-
-                                                    <div class="d-flex  m-2">
-                                                        <div class="container-fluid p-0 d-inline-flex">
-                                                            Status: <?php switch ($ads['AdStatus']) {
-                                                                        case "Pending Review":
-                                                                        case "Pending Payment":
-                                                                            echo '<p class="text-warning">';
-                                                                            break;
-                                                                        case "Rejected Request":
-                                                                        case "Rejected Payment":
-                                                                        case "Cancelled":
-                                                                        case "Expired":
-                                                                            echo '<p class="text-danger">';
-                                                                            break;
-                                                                        case "Approved":
-                                                                            echo '<p class="text-success">';
-                                                                            break;
-                                                                    }
-                                                                    echo $ads['AdStatus'];
-                                                                    ?> </p>
-                                                        </div>
-                                                        <div class="container text-end">
-                                                            <?php if (($ads['AdStatus'] == "Pending Payment" || $ads['AdStatus'] == "Rejected Payment") && $ads['AdStatus'] != "Approved" && $ads['AdStatus'] != "Cancelled") : ?>
-                                                                <button class="btn btn-outline-success m-1" data-bs-target="#payment-<?= $ads['AdID'] ?>" data-bs-toggle="modal" aria-labelledby="exampleModalToggleLabel2"><?php echo ($ads['AdStatus'] == "Pending Payment") ? "Payment" : "Resubmit"; ?></button>
-                                                            <?php endif; ?>
-                                                            <?php if ($ads['AdStatus'] != "Expired" && $ads['AdStatus'] != "Rejected Request" && $ads['AdStatus'] != "Cancelled") : ?>
-                                                                <button class="btn btn-outline-danger m-1" data-bs-target="#cancel-<?= $ads['AdID'] ?>" data-bs-toggle="modal">Cancel Ad</button>
-                                                            <?php endif; ?>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                            <?php }
-                            endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <?php
-    $result = $classified->getAds(NULL, NULL, NULL, isset($_SESSION['auth_user'])?$_SESSION['auth_user']['user_id']:NULL); //Pending Review, Rejected Request, Pending Payment, Rejected Payment, Approved, Cancelled, Expired
-    if (mysqli_num_rows($result) > 0) :
-        while ($ads = $result->fetch_assoc()) {
-    ?>
-
-            <?php if (($ads['AdStatus'] == "Pending Payment" || $ads['AdStatus'] == "Rejected Payment") && $ads['AdStatus'] != "Approved" && $ads['AdStatus'] != "Cancelled") : ?>
-                <!--*PAYMENT MODAL-->
-                <div class="modal fade" id="payment-<?= $ads['AdID'] ?>" aria-hidden="true" aria-labelledby="payment-<?= $ads['AdID'] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
-                    <div class="modal-dialog modal-md modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="payment-<?= $ads['AdID'] ?>"><?php echo ($ads['AdStatus'] == "Pending Payment") ? "Payment" : "Resubmit Payment"; ?></h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="CloseModalPayment"></button>
-                            </div>
-                            <div class="modal-body">
-
-                                Pay for: <?= $ads['AdName'] ?>
-                                <form action="#" method="#">
-                                    <div class="mb-3">
-                                        <label for="formFile" class="form-label">Upload Image</label>
-                                        <input class="form-control" type="file" id="formFile">
-                                    </div>
-                                    <input type="submit" class="btn btn-outline-success float-end" value="Submit">
-                                </form>
-
-
-                            </div>
-                            <div class="modal-footer">
-                                <button class="btn btn-primary" data-bs-target="#historyModal" data-bs-toggle="modal">Back to History</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--*PAYMENT MODAL-->
-
-            <?php endif; ?>
-
-            <?php if ($ads['AdStatus'] != "Expired" && $ads['AdStatus'] != "Rejected Request" && $ads['AdStatus'] != "Cancelled") : ?>
-                <!--*CANCEL MODAL-->
-                <div class="modal fade" id="cancel-<?= $ads['AdID'] ?>" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
-                    <div class="modal-dialog modal-sm modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Cancel Ad</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="CloseModalCancel"></button>
-                            </div>
-                            <div class="modal-body">
-
-                                Cancel: <?= $ads['AdName'] ?>
-                                <form action="Includes/authActions.php?request=CancelAd" method="POST">
-                                    <input type="hidden" value="<?= $ads['AdID'] ?>" name="AdID">
-                                    <input type="hidden" value="<?= $ads['AdAuthorID'] ?>" name="AdAuthorID">
-                                    <label for="formFile" class="form-label">Are you sure you want to cancel? <br>No refunds will be provided!</label>
-                                    <div class="container-fluid d-flex justify-content-end">
-                                        <input type="submit" class="btn btn-outline-danger mx-2 px-4" value="Yes">
-                                        <button type="button" class="btn btn-outline-warning px-4" data-bs-target="#historyModal" data-bs-toggle="modal">No</button>
-                                    </div>
-                                </form>
-
-
-                            </div>
-                            <div class="modal-footer">
-                                <button class="btn btn-primary" data-bs-target="#historyModal" data-bs-toggle="modal">Back to History</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--*CANCEL MODAL-->
-            <?php endif; ?>
-
-
-    <?php }
-    endif; ?>
-
-    <!--//!HISTORY MODAL-->
-
-    <?php endif;?>
 
 </body>
