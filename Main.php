85739ea1ff044da16d4c2ac97e70c8c5417fd2bd
Yo Wen <teohyowen2003@gmail.com>
2023-10-25 10:52:45 +0800

    made categories burger menu, cancel modal

--- Main.php
+++ Main.php
@@ -36,45 +36,57 @@ $redirect = basename($_SERVER['PHP_SELF']);
 
             <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2">
 
-                <form action="" method="GET">
-                    <div class="text-center mt-5">
-                        <b class="fs-3">Categories</b>
-                        <hr>
-                        <div class="text-start">
+                <nav class="navbar navbar-expand-sm bg-body-tertiary">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span><span style="position:relative; top:2px;">Filter</span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                            <form action="" method="GET">
+                                <div class="text-center mt-5">
+                                    <b class="fs-3">Categories</b>
+                                    <hr>
+                                    <div class="text-start">
 
-                            <?php
-                            $categoriesData = $classified->getCategories();
-                            $result = $categoriesData['result'];
-                            $selectedCategories = $categoriesData['selectedCategories'];
+                                        <?php
+                                        $categoriesData = $classified->getCategories();
+                                        $result = $categoriesData['result'];
+                                        $selectedCategories = $categoriesData['selectedCategories'];
 
-                            if (mysqli_num_rows($result) > 0) :
-                                while ($categories = $result->fetch_assoc()) {
-                                    $categoryName = $categories["Category"];
-                                    $isChecked = in_array($categoryName, $selectedCategories) ? 'checked' : '';
-                            ?>
-                                    <div class="checkbox-wrapper-4 ms-3" id="CateText">
-                                        <input class="inp-cbx" id="<?= $categoryName ?>" type="checkbox" name="category[]" value="<?= $categoryName ?>" <?= $isChecked ?> />
-                                        <label class="cbx" for="<?= $categoryName ?>"><span>
-                                                <svg width="12px" height="10px">
-                                                    <use xlink:href="#check-4"></use>
-                                                </svg></span><span><?= $categoryName ?></span></label>
-                                        <svg class="inline-svg">
-                                            <symbol id="check-4" viewbox="0 0 12 10">
-                                                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                                            </symbol>
-                                        </svg>
+                                        if (mysqli_num_rows($result) > 0) :
+                                            while ($categories = $result->fetch_assoc()) {
+                                                $categoryName = $categories["Category"];
+                                                $isChecked = in_array($categoryName, $selectedCategories) ? 'checked' : '';
+                                        ?>
+                                                <div class="checkbox-wrapper-4 ms-3" id="CateText">
+                                                    <input class="inp-cbx" id="<?= $categoryName ?>" type="checkbox" name="category[]" value="<?= $categoryName ?>" <?= $isChecked ?> />
+                                                    <label class="cbx" for="<?= $categoryName ?>"><span>
+                                                            <svg width="12px" height="10px">
+                                                                <use xlink:href="#check-4"></use>
+                                                            </svg></span><span><?= $categoryName ?></span></label>
+                                                    <svg class="inline-svg">
+                                                        <symbol id="check-4" viewbox="0 0 12 10">
+                                                            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                                                        </symbol>
+                                                    </svg>
+                                                </div>
+                                        <?php
+                                            }
+                                        endif;
+                                        ?>
                                     </div>
-                            <?php
-                                }
-                            endif;
-                            ?>
-                        </div>
-                        <input type="hidden" name="search" value="<?= $key ?>">
-                        <input type="submit" class="button-31 mt-5 mb-5" value="Search">
+                                    <input type="hidden" name="search" value="<?= $key ?>">
+                                    <input type="submit" class="button-31 mt-5 mb-5" value="Search">
 
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </form>
+                </nav>
             </div>
+
+
+
             <!--//!Category-->
 
 
@@ -134,7 +146,7 @@ $redirect = basename($_SERVER['PHP_SELF']);
                                                         <div class="modal-body">
 
                                                             Cancel: <?= $ads['AdName'] ?>
-                                                            <form action="Includes/authActions.php?request=CancelAd&redirect=<?=$redirect?>" method="POST">
+                                                            <form action="Includes/authActions.php?request=CancelAd&redirect=<?= $redirect ?>" method="POST">
                                                                 <label for="formFile" class="form-label">Are you sure you want to cancel? <br>You can't revert this action!</label>
                                                                 <div class="container-fluid d-flex justify-content-end">
                                                                     <input type="hidden" value="<?= $ads['AdID'] ?>" name="AdID">
