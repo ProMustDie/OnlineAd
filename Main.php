6fbcd23a04aed491ed821a8b02dbe9ebf5372e30
Yo Wen <teohyowen2003@gmail.com>
2023-10-30 11:01:08 +0800

    card height in main page for admin and user

--- Main.php
+++ Main.php
@@ -115,121 +115,123 @@ if (!empty($_SERVER['QUERY_STRING'])) {
                                 <div class="ImgContainer m-2">
                                     <img src="<?= $ads['AdPicture'] ?>" class="imgSize card-img-top" id="myImg" data-bs-toggle="modal" data-bs-target="#modalImg-<?= $ads['AdID'] ?>">
                                 </div>
-                                <?php if (isset($_SESSION['auth_user']) && $_SESSION['auth_user']['user_type'] === "Admin") :?>
-                                <div class="card-body" style="height:10rem;">
-                                <?php else: ?>
-                                    <!-- INSERT NON ADMIN HERE -->
-                                <?php endif;?>
-                                    <div class="title d-inline-flex" style="height:3.5rem; width:15rem; overflow: hidden;">
-                                        <h6 class="card-title fw-bold" id="TextHeader" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?= $ads['AdName'] ?>"><?= $ads['AdName'] ?></h6>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item"></li>
-                                        <p class="card-text mb-1" id="TextTime"><small class="text-muted">
-                                                <p class="card-text p-0 m-0" id="TextTime"><small class="text-muted"><?= $ads['UserName'] . " posted on " . $formattedDatetime ?></small></p>
-                                            </small>
-                                            <?php
-                                            if (isset($_SESSION['auth_user']) && $_SESSION['auth_user']['user_type'] === "Admin") :
-
-                                            ?>
-                                        <div class="d-flex justify-content-end m-2">
-                                            <button class="btn btn-outline-danger" data-bs-target="#DeleteModal-<?= $ads['AdID'] ?>" data-bs-toggle="modal">Cancel Ad</button>
+                                <?php if (isset($_SESSION['auth_user']) && $_SESSION['auth_user']['user_type'] === "Admin") : ?>
+                                    <div class="card-body" style="height:13rem;">
+                                    <?php else : ?>
+                                        <div class="card-body" style="height:10rem;">
+                                        <?php endif; ?>
+                                        <div class="title d-inline-flex" style="height:3.5rem; width:15rem; overflow: hidden;">
+                                            <h6 class="card-title fw-bold" id="TextHeader" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?= $ads['AdName'] ?>"><?= $ads['AdName'] ?></h6>
                                         </div>
-                                        </p>
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item"></li>
+                                            <p class="card-text mb-1" id="TextTime"><small class="text-muted">
+                                                    <p class="card-text p-0 m-0" id="TextTime"><small class="text-muted"><?= $ads['UserName'] . " posted on " . $formattedDatetime ?></small></p>
+                                                </small>
+                                                <?php
+                                                if (isset($_SESSION['auth_user']) && $_SESSION['auth_user']['user_type'] === "Admin") :
+
+                                                ?>
+                                            <div class="d-flex justify-content-end m-2">
+                                                <button class="btn btn-outline-danger" data-bs-target="#DeleteModal-<?= $ads['AdID'] ?>" data-bs-toggle="modal">Cancel Ad</button>
+                                            </div>
+                                            </p>
 
 
-                                        <!--*DELETE MODAL-->
-                                        <div class="modal fade" id="DeleteModal-<?= $ads['AdID'] ?>" aria-hidden="true" aria-labelledby="exampleModalToggleLabel5" tabindex="-1">
-                                            <div class="modal-dialog modal-md modal-dialog-centered">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h1 class="modal-title fs-3" id="exampleModalToggleLabel5"><strong>Cancel Ad</strong></h1>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <!--*DELETE MODAL-->
+                                            <div class="modal fade" id="DeleteModal-<?= $ads['AdID'] ?>" aria-hidden="true" aria-labelledby="exampleModalToggleLabel5" tabindex="-1">
+                                                <div class="modal-dialog modal-md modal-dialog-centered">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h1 class="modal-title fs-3" id="exampleModalToggleLabel5"><strong>Cancel Ad</strong></h1>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+
+                                                            <strong class="fs-5">Title: <?= $ads['AdName'] ?></strong><br><br>
+
+                                                            <form action="Includes/authActions.php?request=CancelAd&redirect" method="POST">
+                                                                <label for="formFile" class="form-label text-danger">Are you sure you want to <b><u>cancel?</u></b> <br>You can't revert this action!</label>
+                                                                <div class="container-fluid d-flex justify-content-end">
+                                                                    <input type="hidden" value="<?= $ads['AdID'] ?>" name="AdID">
+                                                                    <input type="hidden" value="<?= $redirect ?>" name="redirect">
+                                                                    <input type="submit" class="btn btn-outline-danger mx-2 px-4" value="Yes">
+                                                                    <button type="button" class="btn btn-outline-warning px-4" data-bs-dismiss="modal" aria-label="Close">No</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
                                                     </div>
-                                                    <div class="modal-body">
-
-                                                        <strong class="fs-5">Title: <?= $ads['AdName'] ?></strong><br><br>
-
-                                                        <form action="Includes/authActions.php?request=CancelAd&redirect" method="POST">
-                                                            <label for="formFile" class="form-label text-danger">Are you sure you want to <b><u>cancel?</u></b> <br>You can't revert this action!</label>
-                                                            <div class="container-fluid d-flex justify-content-end">
-                                                                <input type="hidden" value="<?= $ads['AdID'] ?>" name="AdID">
-                                                                <input type="hidden" value="<?= $redirect ?>" name="redirect">
-                                                                <input type="submit" class="btn btn-outline-danger mx-2 px-4" value="Yes">
-                                                                <button type="button" class="btn btn-outline-warning px-4" data-bs-dismiss="modal" aria-label="Close">No</button>
-                                                            </div>
-                                                        </form>
-                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <!--*DELETE MODAL-->
+                                            <!--*DELETE MODAL-->
 
-                                    <?php endif; ?>
-                                    </ul>
-                                </div>
-                            </div>
-
-
-
-
-                            <!--//!MODAL FOR IMAGES POPUP-->
-
-                            <div class="modal fade p-0" id="modalImg-<?= $ads['AdID'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-xl mt-1">
-                                    <div class="modal-content modal-xl">
-                                        <div class="modal-header p-3">
-
-                                            <div class="col">
-                                                <div class="item1">
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="margin-left: 95%;"></button>
-                                                </div>
-                                                <div class="item2">
-                                                    <h1 class="modal-title fs-5" id="exampleModalLabel"><?= $ads['AdName'] ?></h1>
-                                                </div>
-                                                <div class="item3">
-                                                    <span class="text-muted mx-auto"><small><?= $ads['UserName'] . " posted on " . $formattedDatetime ?></small></span>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                        <div class="modal-body" style="max-height: 80vh; overflow-y: auto;">
-                                            <img class="modal-content" id="modalImg" src="<?= $ads['AdPicture'] ?>">
-                                        </div>
-                                        <div class="modal-footer">
-
-                                            <div class="container text-center text-break" id="caption">
-                                                <p class="card-text lh-lg" id="TextCate">
-                                                    <?php
-                                                    $categoriesArray = explode(',', $ads['AdCategory']);
-                                                    foreach ($categoriesArray as $category) {
-
-                                                    ?>
-
-                                                        <span class="rounded-3 bg-success text-light px-2 py-1"><?= $category ?></span>
-
-
-                                                    <?php } ?>
-                                                </p>
-                                            </div>
-                                            <div class="container text-center text-break m-auto" id="caption"><?= $ads['AdDescription'] ?></div>
+                                        <?php endif; ?>
+                                        </ul>
                                         </div>
                                     </div>
-                                </div>
+
+
+
+
+                                    <!--//!MODAL FOR IMAGES POPUP-->
+
+                                    <div class="modal fade p-0" id="modalImg-<?= $ads['AdID'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-xl mt-1">
+                                            <div class="modal-content modal-xl">
+                                                <div class="modal-header p-3">
+
+                                                    <div class="col">
+                                                        <div class="item1">
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="margin-left: 95%;"></button>
+                                                        </div>
+                                                        <div class="item2">
+                                                            <h1 class="modal-title fs-5" id="exampleModalLabel"><?= $ads['AdName'] ?></h1>
+                                                        </div>
+                                                        <div class="item3">
+                                                            <span class="text-muted mx-auto"><small><?= $ads['UserName'] . " posted on " . $formattedDatetime ?></small></span>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                <div class="modal-body" style="max-height: 80vh; overflow-y: auto;">
+                                                    <img class="modal-content" id="modalImg" src="<?= $ads['AdPicture'] ?>">
+                                                </div>
+                                                <div class="modal-footer">
+
+                                                    <div class="container text-center text-break" id="caption">
+                                                        <p class="card-text lh-lg" id="TextCate">
+                                                            <?php
+                                                            $categoriesArray = explode(',', $ads['AdCategory']);
+                                                            foreach ($categoriesArray as $category) {
+
+                                                            ?>
+
+                                                                <span class="rounded-3 bg-success text-light px-2 py-1"><?= $category ?></span>
+
+
+                                                            <?php } ?>
+                                                        </p>
+                                                    </div>
+                                                    <div class="container text-center text-break m-auto" id="caption"><?= $ads['AdDescription'] ?></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                <?php }
+                        else :
+                                ?>
+                                <span class="h2 text-center text-secondary mt-5">No Advertisement Request or Post was found!</span>
+
+                            <?php endif; ?>
+
+
+
+                            <!--//!Images-->
+
+
                             </div>
-
-
-                        <?php }
-                    else :
-                        ?>
-                        <span class="h2 text-center text-secondary mt-5">No Advertisement Request or Post was found!</span>
-
-                    <?php endif; ?>
-
-
-
-                    <!--//!Images-->
-
 
                 </div>
 
@@ -237,15 +239,13 @@ if (!empty($_SERVER['QUERY_STRING'])) {
 
         </div>
 
-    </div>
+        <!--Main Body-->
 
-    <!--Main Body-->
-
-    <!--footer-->
-    <?php
-    include("Includes/footer.php");
-    ?>
-    <!--footer-->
+        <!--footer-->
+        <?php
+        include("Includes/footer.php");
+        ?>
+        <!--footer-->
 
 </body>
 
