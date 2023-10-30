a1197623761a17decf44457ef76d5f3bad272827
Ausca <ausca33@gmail.com>
2023-10-30 10:50:14 +0800

    admin only card height

--- Main.php
+++ Main.php
@@ -115,7 +115,11 @@ if (!empty($_SERVER['QUERY_STRING'])) {
                                 <div class="ImgContainer m-2">
                                     <img src="<?= $ads['AdPicture'] ?>" class="imgSize card-img-top" id="myImg" data-bs-toggle="modal" data-bs-target="#modalImg-<?= $ads['AdID'] ?>">
                                 </div>
+                                <?php if (isset($_SESSION['auth_user']) && $_SESSION['auth_user']['user_type'] === "Admin") :?>
                                 <div class="card-body" style="height:10rem;">
+                                <?php else: ?>
+                                    <!-- INSERT NON ADMIN HERE -->
+                                <?php endif;?>
                                     <div class="title d-inline-flex" style="height:3.5rem; width:15rem; overflow: hidden;">
                                         <h6 class="card-title fw-bold" id="TextHeader" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?= $ads['AdName'] ?>"><?= $ads['AdName'] ?></h6>
                                     </div>
