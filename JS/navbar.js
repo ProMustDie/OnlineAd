a56c0858e18932ac9756aed5213d006252f27b84
Yo Wen <teohyowen2003@gmail.com>
2023-11-30 14:53:03 +0800

    fixed navbar logo logic

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,39 +1,33 @@
-var nav = $('#topnav'); // Assuming you have a nav with the ID 'topnav'
+var navtop = $('#topnav'); // Assuming you have a nav with the ID 'topnav'
 var navImg = $('#topnav'); // Assuming you have a nav with the ID 'topnav'
-var BtmImg = $('#BtmNavImg')
-var BtmNav = $('#BtmNav')
+var BtmImg = $('#BtmNavImg');
+var BtmNav = $('#BtmNav');
 var specificWidth = 768; // Set your specific width here
 
 
+// Function to handle scroll and resize events
+function handleScrollAndResize() {
 
-
-    function checkScreenWidth() {
-        if ($(window).width() > specificWidth) {
-            nav.addClass('show');
-            BtmImg.addClass('hide');
-        } else {
-            nav.removeClass('show');
-            BtmImg.removeClass('hide');
-        }
+    if ($(window).width() > specificWidth && $(window).scrollTop() < 100) {
+        navtop.addClass('show');
+        BtmImg.addClass('hide');
+        BtmNav.removeClass('fixed-top');
+    } else if ($(window).width() > specificWidth) {
+        navtop.removeClass('show');
+        BtmImg.removeClass('hide');
+        BtmNav.addClass('fixed-top');
+    } else {
+        navtop.removeClass('show');
+        BtmImg.removeClass('hide');
+        BtmNav.addClass('fixed-top');
     }
-
-    // Call the function on page load
-    checkScreenWidth();
-
-    // Check the screen width on window resize
-    $(window).resize(function() {
-        checkScreenWidth();
-    });
+}
 
 
-    
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 100) {
-            BtmNav.addClass('fixed-top');
-            BtmImg.removeClass('hide');
-        } else {
-            BtmNav.removeClass('fixed-top');
-            
-        }
-    });
+// Initial call on page load
+handleScrollAndResize();
 
+// Scroll and Resize event handlers
+$(window).on('scroll resize', function() {
+    handleScrollAndResize();
+});
